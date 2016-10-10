dea59cb5908b3ccaacdf565195d12517da416898
Riley Shelton <rs854@nau.edu>
2016-10-09 22:11:58 -0700

    Additonal Layouts for E,M,H

--- app/src/main/java/com/rileygrant/anagram/Easy.java
+++ app/src/main/java/com/rileygrant/anagram/Easy.java
@@ -1,7 +1,15 @@
 package com.rileygrant.anagram;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Easy extends AppCompatActivity {
 
@@ -9,5 +17,10 @@ public class Easy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy);
+    }
+
+    public void onClickSubmit(View v){
+        Intent mintent = new Intent(Easy.this, Results.class);
+        startActivity(mintent);
     }
 }
